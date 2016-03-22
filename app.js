2c4d5b839d71c9d2b1f9e0c606e8523c600b9f0b
Victor <victoraweb@gmail.com>
2016-03-22 16:04:58 -0300

    Filtering with query string

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
+// Modules
 var express = require('express');
+var mongoose = require('mongoose');
+
+// DB connection
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+// Definitions
 var app = express();
 var port = process.env.PORT || 3000;
+var booksRouter = express.Router();
 
+// Models
+var Book = require('./models/bookModel');
 
-app.get('/', function(req, res) {
-    res.send('Welcome to my api.');
-});
+// Routes
+app.use('/api', booksRouter);
+booksRouter.route('/books')
+    .get(function(req, res) {
+        var query = {};
 
+        if(req.query.genre) {
+            query.genre = req.query.genre;
+        }
+        
+        Book.find(query, function (err, books) {
+            if(err) throw err;
+            res.json(books);
+        })
+        // res.render('viewName'); // For html
+    });
+
+// Server
 app.listen(port, function () {
     console.log('Gulp is running at http://localhost:' + port);
 });
