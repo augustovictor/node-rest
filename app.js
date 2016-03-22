3e50f66cf5dbb35552e7326d10e7e44bc717f31d
Victor <victoraweb@gmail.com>
2016-03-22 16:09:05 -0300

    Get a single book

--- app.js
+++ app.js
@@ -22,12 +22,20 @@ booksRouter.route('/books')
         if(req.query.genre) {
             query.genre = req.query.genre;
         }
-        
+
         Book.find(query, function (err, books) {
             if(err) throw err;
             res.json(books);
         })
         // res.render('viewName'); // For html
+    });
+
+booksRouter.route('/books/:id')
+    .get(function(req, res) {
+        Book.findById(req.params.id, function(err, book) {
+            if(err) throw err;
+            res.json(book);
+        });
     });
 
 // Server
