201e14d78fdcf7e95af622742bbe351f8e93cd27
Victor <victoraweb@gmail.com>
2016-03-22 21:47:25 -0300

    Routes clean up

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,5 +1,20 @@
 // Revealing module pattern
 var bookController = function(Book) {
+
+    var mdFindById = function(req, res, next) {
+
+        Book.findById(req.params.id, function(err, book) {
+            if (err) throw err;
+
+            else if (book) {
+                req.book = book;
+                next();
+            }
+
+            else res.status(404).send('No book found');
+        });
+    };
+
     var get = function(req, res) {
         var query = {};
 
@@ -15,16 +30,66 @@ var bookController = function(Book) {
     };
 
     var post = function(req, res) {
-        var book = new Book(req.body);
-        book.save();
 
-        res.status(201).send(book); // Added
+        if (!req.body.title) {
+            res.status(400);
+            res.send('Title is required');
+        }
 
+        else {
+            var book = new Book(req.body);
+            book.save();
+
+            res.status(201);
+            res.send(book); // Added
+        };
+
+    };
+
+    var getOne = function(req, res) {
+        res.json(req.book);
+    };
+
+    var put = function(req, res) {
+        req.book.title  = req.body.title;
+        req.book.author = req.body.author;
+        req.book.genro  = req.body.genro;
+        req.book.read   = req.body.read;
+
+        req.book.save(function(err) {
+            if(err) res.status(500).send(err);
+            else res.json(req.book);
+        });
+    };
+
+    var patch = function(req, res) {
+        if (req.body._id) delete req.body._id;
+
+        for(var param in req.body) {
+            req.book[param] = req.body[param];
+        }
+
+        req.book.save(function(err) {
+            if(err) res.status(500).send(err);
+            else res.json(req.book);
+        });
+    };
+
+    var remove = function(req, res) {
+        req.book.remove(function(err) {
+            if(err) res.status(500).send(err);
+            else res.status(204).send('Removed'); // Removed
+        });
     };
 
     return {
         get: get,
-        post: post
+        getOne: getOne,
+        post: post,
+        put: put,
+        patch: patch,
+        remove: remove,
+        mdFindById: mdFindById
     }
 };
 
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -4,61 +4,19 @@ var routes = function(Book) {
     var bookRouter = express.Router();
     var bookController = require('../controllers/bookController')(Book);
 
+    // Middleware
+    bookRouter.use('/:id', bookController.mdFindById);
+
+    // Routes
     bookRouter.route('/')
         .get(bookController.get)
-
         .post(bookController.post);
 
-    // Middleware
-    bookRouter.use('/:id', function(req, res, next) {
-        Book.findById(req.params.id, function(err, book) {
-            if (err) throw err;
-
-            else if (book) {
-                req.book = book;
-                next();
-            }
-
-            else res.status(404).send('No book found');
-        });
-    });
-
     bookRouter.route('/:id')
-        .get(function(req, res) {
-            res.json(req.book);
-        })
-
-        .put(function(req, res) {
-            req.book.title  = req.body.title;
-            req.book.author = req.body.author;
-            req.book.genro  = req.body.genro;
-            req.book.read   = req.body.read;
-
-            req.book.save(function(err) {
-                if(err) res.status(500).send(err);
-                else res.json(req.book);
-            });
-        })
-
-        .patch(function(req, res) {
-            if (req.body._id) delete req.body._id;
-
-            for(var param in req.body) {
-                req.book[param] = req.body[param];
-            }
-
-            req.book.save(function(err) {
-                if(err) res.status(500).send(err);
-                else res.json(req.book);
-            });
-        })
-
-        .delete(function(req, res) {
-            req.book.remove(function(err) {
-                if(err) res.status(500).send(err);
-                else res.status(204).send('Removed'); // Removed
-            });
-        });
+        .get(bookController.getOne)
+        .put(bookController.put)
+        .patch(bookController.patch)
+        .delete(bookController.remove);
 
     return bookRouter;
 };
